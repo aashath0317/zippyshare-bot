af0361aecb6cb9880945ac8294232f943718f23f
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-12-26 16:44:22 +0545

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,32 +1,33 @@
+
 import subprocess
+import os
 from pyrogram import Client, filters
-bot = Client("name", api_id="3030128", api_hash="cfc3885f5d2cbdbc5f10e6a643de2711",bot_token="2141733603:AAFmyuyeseVJZh3geZ83WJfT6yfizdpJagk")
+import time
+bot = Client("name", api_id="3030128", api_hash="cfc3885f5d2cbdbc5f10e6a643de2711",bot_token="1976201011:AAHMTReyFo2b81UD_nao3S41B-M602PFMxs")
 
-
-chat_id = 12734305463
- list_png=[]
-  
- for f in sorted(os.listdir(DIR_WITH_FILES)):
-     if (str(f))[-3:] == "png":
-        list_png.append(f)
-         png_with_path=DIR_WITH_FILES+str(f)
-         list_png_with_path.append(png_with_path)
-    
- print(list_png)
-
-
-bot.send_message(chat_id, text= "Downloading......" )
-        prin = subprocess.run(["zippyshare-dl", link], capture_output=True, text=True).stdout.strip("\n")
-        x=prin.split("\n")
-        name = x[2].split(":")[1].strip(" ")
-        size = x[3].split(":")[1]
-        bot.send_message(chat_id, text= f"Download Success..... \nFile info \n\nFile Name = {name}\nSize = {size}" )
-        bot.send_message(chat_id, text= "Uploading to telegram.....")
-        x=prin.split("\n")
-        bot.send_document(chat_id, document=name)
-        subprocess.run(["rm", name])
-    else:
-        bot.send_message(chat_id, text="You are not allowed to use this bot")    
-    print(prin)
-
+filelist=os.listdir("/home/akeelaashath2")
+for fichier in filelist[:]: # filelist[:] makes a copy of filelist.
+    if not(fichier.endswith(".png")):
+        filelist.remove(fichier)
+loop = len(filelist)
+looper = 0
+@bot.on_message(filters.private)
+def start(Client, message):
+    while 1:
+        filelist=os.listdir("/home/akeelaashath2")
+        for fichier in filelist[:]: # filelist[:] makes a copy of filelist.
+            if not(fichier.endswith(".png")):
+                filelist.remove(fichier)
+        loop = len(filelist)
+        looper = 0        
+        chat_id = int(message.chat.id)
+        while loop > looper:
+            name = filelist[looper]
+            looper = looper+1
+            if chat_id == 1273430546:
+                bot.send_document(chat_id, document=name)
+                subprocess.run(["rm", name])
+            else:
+                bot.send_message(chat_id, text="You are not allowed to use this bot")   
+        time.sleep(5)
 bot.run()
